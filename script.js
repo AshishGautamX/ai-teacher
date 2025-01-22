2b6377f28117b279a67012b9b49ed829c13de7cf
AshishGautamX <ashishgautam835@gmail.com>
2025-01-22 18:36:42 +0530

    Add files via upload
    SCRIPT

---
+++ script.js
@@ -0,0 +1,4 @@
+function personalizeContent() {
+    const stream = document.getElementById('stream').value;
+    alert(`Your syllabus-based resources are being prepared for the ${stream.toUpperCase()} stream!`);
+}
