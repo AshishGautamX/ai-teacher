244c889486f4651533e6dcd6e9e4466da607b2f7
AshishGautamX <ashishgautam835@gmail.com>
2025-02-01 21:01:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     const googleSigninButton = document.getElementById('google-signin');
     const signupForm = document.getElementById('signup-form');
-    
+    const studentForm = document.getElementById('student-form');
+    const teacherForm = document.getElementById('teacher-form');
+    const roleRadios = document.querySelectorAll('input[name="role"]');
 
+    roleRadios.forEach(radio => {
+        radio.addEventListener('change', function () {
+          if (this.value === 'student') {
+            studentForm.style.display = 'block';
+            teacherForm.style.display = 'none';
+          } else {
+            studentForm.style.display = 'none';
+            teacherForm.style.display = 'block';
+          }
+        });
 
+    });
     // Basic form validation
     signupForm.addEventListener('submit', (e) => {
         const role = document.querySelector('input[name="role"]:checked');
@@ -22,9 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Please select your role');
         }
       });
-
-
-
     
 });
 
